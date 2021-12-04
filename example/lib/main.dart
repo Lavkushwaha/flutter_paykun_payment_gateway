d7861749a1bda636cbbee65d687c3e7fccb12fbd
Lavkant Kachhwaha <kush373@gmail.com>
2021-12-04 23:04:19 +0530

    testing changes added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,29 +21,20 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
+  Future<void> startPayment() async {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await FlutterPaykun.platformVersion ?? 'Unknown platform version';
+      await FlutterPaykun.startPayment();
     } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      throw 'Failed to get platform version.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -54,7 +45,12 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: ElevatedButton(
+            child: const Text("Start Payment"),
+            onPressed: () {
+              startPayment();
+            },
+          ),
         ),
       ),
     );
--- lib/flutter_paykun.dart
+++ lib/flutter_paykun.dart
@@ -1,10 +1,20 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class FlutterPaykun {
   static const MethodChannel _channel = MethodChannel('flutter_paykun');
+
+  static Future<bool> get configure async {
+    // final String version = await _channel.invokeMethod('getPlatformVersion');
+    // return version;
+    return false;
+  }
+
+  static Future startPayment() async {
+    final String result = await _channel.invokeMethod('startPayment');
+    return result;
+  }
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
